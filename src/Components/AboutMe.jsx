17dcdddf86bdc63efdee8f8305dd9769b4710f14
Edwin <edwincastaneda0801@gmail.com>
2024-11-21 11:24:44 -0600

    icon slider and aboout container done

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useRef, useState } from "react";
 import "../styles/AboutMe.css";
 
 export default function AboutMe() {
+  //   const slideRef = useRef(null);
+  //   const [currentIndex, setCurrentIndex] = useState(0);
+
+  //   useEffect(() => {
+  //     const slider = slideRef.current;
+  //     const icons = slider.children;
+  //     const iconWidth = icons[0].offsetWidth + parseFloat(getComputedStyle(slider).gap)
+  //     const totalIcons = icons.length;
+
+  //   })
+
   return (
     <div className="about-container">
       <h2>Some words about me</h2>
@@ -16,6 +28,55 @@ export default function AboutMe() {
         I get it right and bringing ideas to life through code.
       </p>
       <p>
+        <div className="slider-container">
+          <div className="slider">
+            <img
+              className="slider-icon js"
+              src="../../public/icons/js-icon.png"
+              alt="javascripticon"
+            />
+            <img
+              className="slider-icon"
+              src="../../public/icons/react-icon.png"
+              alt="react icon"
+            />
+            <img
+              className="slider-icon"
+              src="../../public/icons/html-icon.png"
+              alt="html icon"
+            />
+            <img
+              className="slider-icon"
+              src="../../public/icons/css-icon.png"
+              alt="css icon"
+            />
+            <img
+              className="slider-icon"
+              src="../../public/icons/redux-icon.png"
+              alt="redux icon"
+            />
+            <img
+              className="slider-icon"
+              src="../../public/icons/express-icon.png"
+              alt="express icon"
+            />
+            <img
+              className="slider-icon"
+              src="../../public/icons/nodejs-icon.png"
+              alt="nodejs icon"
+            />
+            <img
+              className="slider-icon"
+              src="../../public/icons/git-icon.png"
+              alt="git icon"
+            />
+            <img
+              className="slider-icon"
+              src="../../public/icons/github-icon.png"
+              alt="github icon"
+            />
+          </div>
+        </div>
         <span className="big-letter">W</span>hen I'm not coding, I enjoy running
         to clear my head, gaming for fun and strategy, and immersing myself in
         the world of music and movies to relax. I'm also an audiobook enthusiast
@@ -24,6 +85,10 @@ export default function AboutMe() {
         my love of multitasking, creativity and balance in life, which is also
         reflected in my approach to work and problem solving.
       </p>
+
+      <a className="transparent-button cv-button" role="button" href="">
+        Download CV
+      </a>
     </div>
   );
 }
